8c017f171ae8187c08cf89ee03368ad8f258ee33
Tauno Tamm <57254922+taunotamm@users.noreply.github.com>
2020-04-29 21:26:44 +0300

    lisasin juhusliku sõna genereerija

--- Poomismang/src/main/java/Peaklass.java
+++ Poomismang/src/main/java/Peaklass.java
@@ -1,7 +1,20 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Peaklass {
+
+
+    public static String juhuslikSõna (ArrayList<String> sõnad){
+        int sõnadeArvListiIndeksina = sõnad.size() - 1;
+        return sõnad.get((int) (Math.random()*sõnadeArvListiIndeksina));
+    }
+
+
     public static void main(String[] args) throws IOException {
-        System.out.println(andmebaas.loebSisse("sonad.txt"));
+        ArrayList<String> sõnad = andmebaas.loebSisse("sonad.txt");
+        System.out.println(sõnad);
+        System.out.println(juhuslikSõna(sõnad));
+
     }
 }
